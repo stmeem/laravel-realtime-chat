235af97011675db826f2c665ba1f9c14046ba0f7
stmeem <stmeem@yahoo.com>
2022-11-06 14:11:32 +0600

    Create visual components to show messages

---
+++ resources/views/chat/chatroom.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+@push('styles')
+    <style>
+
+    </style>
+@endpush
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Real-time Chat App</div>
+
+                    <div class="card-body">
+                        <div class="row p-2">
+                            <div class="col-10">
+                                <div class="row">
+                                    <div class="col-12 border rounded-lg p-3">
+                                        <ul id="messages" class="list-unstyled overflow-auto" style="height: 45vh">
+                                            <li>Test1:Hello</li>
+                                            <li>Test2:Hi!</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                    <form>
+                                        <div class="row py-3">
+                                            <div class="col-10">
+                                                <input id="message" class="form-control" type="text">
+                                            </div>
+                                            <div class="col-2">
+                                                <button id="send" type="submit" class="btn btn-info text-white">Send</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                            </div>
+                            <div class="col-2">
+                                <p><strong>Online now</strong></p>
+                                <ul id="users" class="list-unstyled overflow-auto text-info" style="height: 45vh">
+                                    <li>Test1</li>
+                                    <li>Test2</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+
+@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @stack('styles')
 </head>
 <body>
     <div id="app">
@@ -76,5 +77,6 @@
             @yield('content')
         </main>
     </div>
+    @stack('scripts')
 </body>
 </html>
