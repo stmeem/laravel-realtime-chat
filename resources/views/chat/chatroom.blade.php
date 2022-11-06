6baa5934aa5b86a7442715b937763e55aa9df7be
stmeem <stmeem@yahoo.com>
2022-11-06 15:48:02 +0600

    Manage users to join and leave the channel

--- resources/views/chat/chatroom.blade.php
+++ resources/views/chat/chatroom.blade.php
@@ -59,7 +59,15 @@
                 usersElement.appendChild(element)
             })
         })
-        .joining()
-        .leaving()
+        .joining((user)=>{
+            let element= document.createElement('li')
+            element.setAttribute('id',user.id)
+            element.innerText = user.name;
+            usersElement.appendChild(element)
+        })
+        .leaving((user)=>{
+            const element = document.getElementById(user.id)
+            element.parentNode.removeChild(element)
+        })
 </script>
 @endpush
